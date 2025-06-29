b05318d16348fcef63afb870ce310342f2382b9c
Cristian Penteado <cristianpenteado@proton.me>
2025-06-29 18:55:58 -0300

    adicionada validação global de not found

--- src/main/java/io/github/cristianpenteado/crud_produtos/exception/ResourceNotFoundException.java
+++ src/main/java/io/github/cristianpenteado/crud_produtos/exception/ResourceNotFoundException.java
@@ -1,10 +1,9 @@
 package io.github.cristianpenteado.crud_produtos.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
         super(message);
